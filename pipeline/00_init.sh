012af648e63577e15199a8e4b69d206dc1440d0f
Jason Stajich <jasonstajich.phd@gmail.com>
2026-05-24 09:32:50 -0700

    new folder structure

--- pipeline/00_init.sh
+++ pipeline/00_init.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/bash -l
 
-mkdir -p dna pep cds gff3
+mkdir -p data
+pushd data
+mkdir -p dna pep cds gff3 trna
+popd
 SAMPLES=samples.csv
 INDIR=$1
 if [ -z "$INDIR" ]; then 
-  INDIR=/bigdata/stajichlab/shared/projects/1KFG/common_annotate
+	INDIR=genome_annotation
 fi
+INDIR=$(realpath $INDIR)
 IFS=,
 tail -n +2 $SAMPLES | while read ASMID SPECIES_IN STRAIN BIOPROJECT NCBI_TAXONID BUSCO_LINEAGE PHYLUM SUBPHYLUM CLASS SUBCLASS ORDER FAMILY GENUS SPECIES TRANSL_TABLE LOCUSTAG
 do
 	TAG=$(echo -n "$SPECIES $STRAIN" | perl -p -e 's/ /_/g;')
-	if [ ! -d "$INDIR/annotate/$TAG" ]; then
+	if [ ! -d "$INDIR/$TAG" ]; then
 		echo "cannot find input files $SPECIES $STRAIN $ASMID"
 	fi
-	ln -sf $INDIR/annotate/$TAG/predict_results/${TAG}.gff3 gff3/
-	ln -sf $INDIR/annotate/$TAG/predict_results/${TAG}.proteins.fa pep/
-	ln -sf $INDIR/annotate/$TAG/predict_results/${TAG}.cds-transcripts.fa cds/
-	ln -sf $INDIR/annotate/$TAG/predict_results/${TAG}.scaffolds.fa dna/
+	ln -sf $INDIR/$TAG/predict_results/${TAG}.gff3 data/gff3/
+	ln -sf $INDIR/$TAG/predict_results/${TAG}.proteins.fa data/pep/
+	ln -sf $INDIR/$TAG/predict_results/${TAG}.cds-transcripts.fa data/cds/
+	ln -sf $INDIR/$TAG/predict_results/${TAG}.scaffolds.fa data/dna/
+	ln -sf $INDIR/$TAG/predict_misc/trnascan.no-overlaps.gff3 data/trna/${TAG}.tRNA.gff3
 done
